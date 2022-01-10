534dfdc72315b13996bce787ff7e13d8f882b9f1
sourav55555 <souravsinghassr555@gmail.com>
2022-01-10 11:02:22 +0600

    chellange in node

---
+++ backend.js
@@ -0,0 +1,31 @@
+const express= require('express');
+const app= express();
+const path= require('path');
+const hbs= require('hbs');
+
+
+const staticPath= path.join(__dirname,"./public");
+const templatePath= path.join(__dirname,"./templates/views");
+const partialPath= path.join(__dirname,"./templates/partials");
+
+app.use(express.static(staticPath));
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+hbs.registerPartials(partialPath);
+
+app.get("",(req,res)=>{
+    res.render('index');
+})
+app.get("/crew",(req,res)=>{
+   res.render('crew');  
+})
+app.get("/destination",(req,res)=>{
+    res.render('destination');
+})
+app.get("/technology",(req,res)=>{
+    res.render('technology');
+})
+
+app.listen(5000,()=>{
+    console.log('listening to the port')
+})
